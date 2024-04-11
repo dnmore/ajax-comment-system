a975ae0d455f6030568053ad035f0daa620b2635
Diana Moretti <morettidn@gmail.com>
2024-04-11 12:18:39 +0200

    adding errors handling

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -1,5 +1,41 @@
 const loadButton = document.getElementById("load-btn");
-const commentsSection = document.getElementById("comments-container");
+const commentsContainer = document.getElementById("comments-container");
+const commentsForm = document.getElementById("comments-form");
+const commentName = document.getElementById("name");
+const commentText = document.getElementById("comment");
+const dialog = document.querySelector("dialog");
+const dialogButton = document.getElementById("close-dialog");
+const errorDescription = document.getElementById("error-description");
+
+async function saveComment(event) {
+  event.preventDefault();
+
+  const userName = commentName.value;
+  const userComment = commentText.value;
+
+  const enteredComment = { name: userName, comment: userComment };
+
+  try {
+    const response = await fetch("/comments", {
+      method: "POST",
+      body: JSON.stringify(enteredComment),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      fetchComments();
+    } else {
+      errorDescription.textContent = "Could not send comment!";
+      dialog.showModal();
+    }
+  } catch (error) {
+    errorDescription.textContent =
+      "Could not send request - try again later!";
+    dialog.showModal();
+  }
+}
 
 function displayComments(comments) {
   const commentsList = document.createElement("ul");
@@ -14,12 +50,35 @@ function displayComments(comments) {
 }
 
 async function fetchComments() {
-  const response = await fetch("/comments");
-  const data = await response.json();
+  try {
+    const response = await fetch("/comments");
+    if (!response.ok) {
+      errorDescription.textContent = "Fetching comments failed!";
+      dialog.showModal();
+      return;
+    }
 
-  const commentsListElement = displayComments(data);
-  commentsSection.innerHTML = "";
-  commentsSection.appendChild(commentsListElement);
+    const data = await response.json();
+
+    if (data && data.length > 0) {
+      const commentsListElement = displayComments(data);
+      commentsContainer.innerHTML = "";
+      commentsContainer.appendChild(commentsListElement);
+    } else {
+      const emptyMessage = document.createElement("p");
+      emptyMessage.textContent =
+        "We could not find any comments yet. Be the first one to add one!";
+      commentsContainer.appendChild(emptyMessage);
+    }
+  } catch (error) {
+    errorDescription.textContent = "Fetching comments failed";
+    dialog.showModal();
+  }
 }
 
+dialogButton.addEventListener("click", () => {
+  dialog.close();
+});
+
+commentsForm.addEventListener("submit", saveComment);
 loadButton.addEventListener("click", fetchComments);
--- routes/comments__routes.js
+++ routes/comments__routes.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const mongodb = require("mongodb");
 
 const db = require("../data/database");
 
@@ -9,20 +8,20 @@ router.get("/", async function (req, res) {
   res.render("home");
 });
 
-router.get('/comments', async function(req, res){
-  const comments = await db.getDb().collection('comments').find().toArray();
+router.get("/comments", async function (req, res) {
+  const comments = await db.getDb().collection("comments").find().toArray();
 
   res.json(comments);
-})
+});
 
-router.post('/comments', async function(req, res){
+router.post("/comments", async function (req, res) {
   const newComment = {
     name: req.body.name,
     comment: req.body.comment,
-  }
+  };
 
-  await db.getDb().collection('comments').insertOne(newComment);
-  res.redirect('/')
-})
+  await db.getDb().collection("comments").insertOne(newComment);
+  res.redirect("/");
+});
 
 module.exports = router;
